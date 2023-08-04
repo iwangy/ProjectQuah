cf52c409271165b4571108abd9a65090991f7bb2
Ian Wang <IanWa@mossadams.ma>
2023-08-04 14:18:11 -0700

    v1

--- popup.js
+++ popup.js
@@ -8,29 +8,6 @@ const test = async e => {
     chrome.tabs.sendMessage(activeTab.id, { action: "daily" });
 };
 
-const test2 = async e => {
-    console.log("beep");
-    const activeTab = await getActiveTabURL();
-    console.log(activeTab.id)
-
-    var inputText = document.getElementById('engagementNo').value;
-
-    chrome.tabs.sendMessage(activeTab.id, { action: "engno", text: inputText.trim() });
-}
-
-const test3 = async e => {
-    console.log("brrt");
-    const activeTab = await getActiveTabURL();
-    console.log(activeTab.id)
-    
-    const selectTag = document.getElementById('emsOptions').value;
-    const hours = document.getElementById('hours').value;
-    const desc = document.getElementById('description').value;
-
-
-    chrome.tabs.sendMessage(activeTab.id, {action: "emsop", text: selectTag.trim(), time: hours, desc: desc})
-}
-
 const test4 = async e => {
     console.log("bam");
     const activeTab = await getActiveTabURL();
@@ -76,20 +53,9 @@ document.addEventListener("DOMContentLoaded", async () => {
         const myButton = document.getElementById("myButton");
         myButton.addEventListener("click", test);
 
-        // Eng Input EL
-        // const fillButton = document.getElementById('fillButton');
-        // fillButton.addEventListener('click', test2)
-
-        // EMS EL
-        // const getValue = document.getElementById('emsButton');
-        // getValue.addEventListener('click', test3);
-
         // Verify EL
         const verifyButton = document.getElementById("verify");
         verifyButton.addEventListener('click', test4);
-        // verifyButton.addEventListener("click", test4);
-        // document.querySelector('tr[data-kendo-grid-item-index="1"]')
-        // document.querySelector('tbody').childElementCount; - 1
 
         chrome.runtime.sendMessage({ action: "getVerifyData" }, response => {
             const storedVerifyData = response.data;
@@ -100,13 +66,5 @@ document.addEventListener("DOMContentLoaded", async () => {
         const container = document.getElementsByClassName("container")[0];
         container.innerHTML = '<div class="title"> This is not an iAccess page.</div>';
     }
-
-    document.querySelector('#go-to-options').addEventListener('click', function() {
-        if (chrome.runtime.openOptionsPage) {
-          chrome.runtime.openOptionsPage();
-        } else {
-          window.open(chrome.runtime.getURL('options.html'));
-        }
-      });
 
 });
